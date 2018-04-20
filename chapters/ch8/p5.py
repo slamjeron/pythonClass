0acec22ed56f0bbad8ed9bc3ac5d3bb48120afc2
nerd <elder.angell@gmail.com>
2018-04-20 16:43:46 -0600

    fdf

---
+++ chapters/ch8/p5.py
@@ -0,0 +1,27 @@
+'''
+Develop a terminal-based program that allows a bank manager to manip- ulate the accounts in a bank.
+This menu-driven program should include all of the relevant options,
+such as adding a new account, removing an account, and editing an account.
+'''
+
+
+class account:
+    def __init__(self, fname, lname, accountnum, ballance):
+        self._fname = fname
+        self._lname = lname
+        self._accountNum = accountnum
+        self._ballance = ballance
+accounts = list()
+
+def addAcount(fname='john', lname='do', accountNum=00000, ballance=0, accounts=list() ):
+    accounts.append(account(fname,lname,accountNum,ballance))
+    acSave(accounts)
+    print('you are creating an acount belonging to ',fname,' ',lname,' with the account number ', accountNum,' and a balance of ', ballance)
+
+
+def delleatAcount(fname, lname, acountnum, accounts=list()):
+
+    index=findAcount(fname,lname,accountnum)
+    accounts.remove(index)
+
+    print("are you sure you want to deleat account", acountnum, 'belonging to ', fname, ' ', lname)
